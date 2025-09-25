a6fa390cf375ebbe404ca41ed933e8938db1295d
OkauanDev <teko4278@gmail.com>
2025-09-25 17:32:14 -0300

    iniciando minha jornada em java

---
+++ secao02/Variavel.java
@@ -0,0 +1,12 @@
+package secao02;
+
+public class Variavel {
+    public static void main(String[] args) {
+        String name = "Kauan";
+        System.out.println(name);
+
+        int n1 = 2 ;
+        int n2 = 3;
+        System.out.println(n1 + n2);
+    }
+}
