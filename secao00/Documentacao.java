167f360663daa4c8e435e8a7637b93e16fe9c003
OkauanDev <teko4278@gmail.com>
2025-09-26 18:27:16 -0300

    aprendendo a concatenar strings e aprendendo sobre char, atribuição de variável com outro valor

---
+++ secao00/Documentacao.java
@@ -0,0 +1,18 @@
+package secao00;
+
+public class Documentacao {
+    public static void main(String[] args) {
+        // Anotação de todos os pontos e estudos sobre o java 
+
+        /*
+         * 
+         *  
+         *              STRINGS
+         *  strings são sequencias de caracteres usadas para armazenar e manipular texto
+         *  Classes String: Em Java, as Strings são objetos da classe String e oferecem métodos 
+         *  para manipulação de texto.
+         *  Uma string pode ser alterada, qualquer modificação gera uma nova string.
+         *  Podemos concatenar (unir) duas strings com o operador + .
+         * */;
+    }
+}
--- secao02/Variavel.java
+++ secao02/Variavel.java
@@ -2,11 +2,34 @@ package secao02;
 
 public class Variavel {
     public static void main(String[] args) {
+        // 1 -> o que são variável 
+        // tipo -> atribuir valor
         String name = "Kauan";
         System.out.println(name);
 
         int n1 = 2 ;
         int n2 = 3;
         System.out.println(n1 + n2);
+
+        // 2 -> atribuição de varaivel com outra
+        String teste = "Teste";
+        
+        String testando = teste;
+        System.out.println(testando);
+
+        Long numeroGrande  = 9999999999999999L;
+        System.out.println(numeroGrande);
+
+        // CONCATENAÇÃO DE STRINGS COM O OPERADOR +
+        String firstName = "Isa";
+        String lastName = "Nascimento";
+        System.out.println(firstName + " " + lastName);
+
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+
+        // 5 -> Char 
+        char letra = 'M';
+        System.out.println(letra);
     }
 }
