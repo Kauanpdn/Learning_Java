829aeb908610ea7861deb5e727c8152922358adc
OkauanDev <teko4278@gmail.com>
2025-11-23 15:52:11 -0300

    terminando a sessão de classes básica

--- secao17/Carro.java
+++ secao17/Carro.java
@@ -2,17 +2,47 @@ package secao17;
 
 public class Carro {
     
-    // atributos ou prioridades
+    // atributos ou propriedades
     String marca;
     String modelo;
-    int ano;
+    int ano; // 2010
+
+    double velocidadeAtual = 0;
+    boolean motorLigado = false;
 
     // métodos
-    void acerelar(){
-        System.out.println("Estamos acelerando o carro");
+    void acelerar() {
+        System.out.println("Estamos acelerando o carro!");
     }
 
-    void exibirInfo(){
-        System.out.println("Marca: " + marca + " , modelo: " + modelo + " , ano: " + ano);
+    void exibirInfo() {
+        System.out.println("Marca: " + marca + ", modelo: " + modelo + ", ano: " + ano);
     }
+
+    // 3 - métodos
+    void ligarMotor() {
+
+        if(!motorLigado) {
+
+            motorLigado = true;
+            System.out.println("Ligando motor...");
+
+        } else {
+            System.out.println("O motor já está ligado!");
+        }
+
+    }
+
+    void aumentarVelocidade(double incremento) {
+        if(motorLigado) {
+
+            velocidadeAtual += incremento;
+
+            System.out.println("A velocidade atual é: " + velocidadeAtual);
+
+        } else {
+            System.out.println("Primeiro precisa ligar o motor.");
+        }
+    }
+
 }
